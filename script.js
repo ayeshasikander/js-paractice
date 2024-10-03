d99a92182885038f499695d799bf1da397cbc9a0
Ayesha_Frontend_Web <160005701+Ayesha-Sikander-724@users.noreply.github.com>
2024-10-03 10:39:52 +0500

    plaindrome

---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log("elu elu");
+
+
+// if ("" == false) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+// function display(){
+//     document.writeln(10+20+"30")
+// }
+// display();
+
+// console.log(Boolean([]));
+
+// console.log(+false);
+// console.log(+true);
+
+// let a;
+// a = 10;
+// console.log(a)
+
+
+// function mysteriousSequence(n) {
+//     return Array.from({length: n}, (_, i) => i + 1)
+//         .map(x => x % 15 === 0 ? "FizzBuzz" :
+//                  x % 3 === 0 ? "Fizz" :
+//                  x % 5 === 0 ? "Buzz" : x)
+//         .filter(x => typeof x === "string")
+//         .reduce((acc, curr) => acc + curr.length, 0);
+// }
+
+// console.log(mysteriousSequence(100));
+
