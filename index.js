1d642658cbb7d52f19b18681560591da5ed52190
ayeshasikander <ayesha@724.one>
2024-12-04 13:05:33 +0500

    test some ts code

--- index.js
+++ index.js
@@ -18,15 +18,38 @@
 // console.log(isPalindrome(str3));
 
 
-function plaindrome(str){
-    let word=str.length
-    for(let i=0;i<word/2;i++){
-        if(str[i]!=str[word-1-i]){
-            return false;
-        }
-    }
-    return true;
-}
+// function plaindrome(str){
+//     let word=str.length
+//     for(let i=0;i<word/2;i++){
+//         if(str[i]!=str[word-1-i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-let str1 = "eye";
-console.log(plaindrome(str1))
+// let str1 = "eye";
+// console.log(plaindrome(str1))
+
+// function add(a:number,b:string){
+//     return (a+b)
+// }
+// console.log(add(10,20))
+
+// main.ts(4,20): error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+// [Compilation failed with exit code 2]
+
+// function add2(a: number, b: number) {
+//     return a + b;
+// }
+// console.log(add2("10",20))
+
+// main.ts(4,17): error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+// [Compilation failed with exit code 2]
+
+// function hello(a: number, b: number) {
+//     return a + b;
+// }
+// console.log(hello(10,20))
